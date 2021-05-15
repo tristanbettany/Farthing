a58cd20d15dedf38d54e76af49b0ec805dc04457
Tristan Bettany <tristan.bettany@gmail.com>
2021-05-15 11:25:28 +0100

    Add interface bindings in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,36 @@
 
 namespace App\Providers;
 
+use App\Interfaces\AccountsInterface;
+use App\Interfaces\ServiceInterface;
+use App\Interfaces\TagsInterface;
+use App\Interfaces\TemplatesInterface;
+use App\Interfaces\TransactionsInterface;
+use App\Services\AbstractService;
+use App\Services\AccountsService;
+use App\Services\TagsService;
+use App\Services\TemplatesService;
+use App\Services\TransactionsService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private const BINDINGS = [
+        ServiceInterface::class => AbstractService::class,
+        AccountsInterface::class => AccountsService::class,
+        TagsInterface::class => TagsService::class,
+        TemplatesInterface::class => TemplatesService::class,
+        TransactionsInterface::class => TransactionsService::class,
+    ];
+
     public function register(): void
     {
-        //TODO: Register application services
+        foreach (self::BINDINGS as $interface => $concreteImplementation) {
+            $this->app->bind(
+                $interface,
+                $concreteImplementation
+            );
+        }
     }
 
     public function boot(): void
